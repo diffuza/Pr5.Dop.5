a57d256a86008c3af7eaefb1d7157e3925763772
USER <USER@DESKTOP-TRQG01D>
2022-09-30 19:08:33 +0300

    Добавьте файлы проекта.

---
+++ Pr5.Dop.5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свое имя: "); // вывод на экран строки с просьбой пользователя ввести своё имя.
+            string name = Console.ReadLine(); // присвоение лок.переменной "name" с введённым ранее именем строкового типа "string".
+            Console.WriteLine($"Привет {name}"); // вывод строки "Привет {name}" с указанным ранее именем на экран.
+            Console.ReadKey(); // ожидание нажатия пользователем любой клавиши для закрытия консоли.
+        }
+    }
+}
